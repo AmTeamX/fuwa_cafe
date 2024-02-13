7da1d0594d49b3977971c82a4b81ee5290d15f4a
AmTeamX <amteamxmail@gmail.com>
2024-02-13 22:50:24 +0700

    fix expanded

--- lib/pages/profile/mainprofile/mainprofile.dart
+++ lib/pages/profile/mainprofile/mainprofile.dart
@@ -57,95 +57,91 @@ class _ProfileState extends State<Profile> {
         width: screen.width,
         height: screen.height,
         child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-          Column(
-            children: [
-              Container(
-                width: screen.width,
-                height: screen.height * 0.15,
-                decoration: const BoxDecoration(
-                  color: Color(0xFFFBF6F0),
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(20),
-                      bottomRight: Radius.circular(20)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color(0x40000000),
-                      blurRadius: 4,
-                      offset: Offset(0, 4), // Shadow position
-                    ),
-                  ],
+          Container(
+            width: screen.width,
+            height: screen.height * 0.15,
+            decoration: const BoxDecoration(
+              color: Color(0xFFFBF6F0),
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20)),
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0x40000000),
+                  blurRadius: 4,
+                  offset: Offset(0, 4), // Shadow position
                 ),
-                child: Row(
-                  children: [
-                    IconButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const HomePage()),
-                          );
-                        },
-                        icon: const Icon(Icons.keyboard_arrow_left)),
-                    Text(
-                      "Profile",
-                      style: GoogleFonts.chewy(
-                          textStyle: const TextStyle(
-                              fontSize: 28,
-                              color: Color(0xFF000000),
-                              decoration: TextDecoration.none)),
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
+              ],
+            ),
+            child: Row(
+              children: [
+                IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const HomePage()),
+                      );
+                    },
+                    icon: const Icon(Icons.keyboard_arrow_left)),
+                Text(
+                  "Profile",
+                  style: GoogleFonts.chewy(
+                      textStyle: const TextStyle(
+                          fontSize: 28,
+                          color: Color(0xFF000000),
+                          decoration: TextDecoration.none)),
+                  textAlign: TextAlign.center,
                 ),
-              ),
-              SizedBox(
-                height: screen.height * 0.05,
-              ),
-              GestureDetector(
-                onTap: () async {
-                  await _pickImage();
-                  if (_imageFile != null) {
-                    showDialog(
-                      context: context,
-                      barrierDismissible:
-                          false, // Prevent user from dismissing the dialog
-                      builder: (BuildContext context) {
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      },
+              ],
+            ),
+          ),
+          SizedBox(
+            height: screen.height * 0.05,
+          ),
+          GestureDetector(
+            onTap: () async {
+              await _pickImage();
+              if (_imageFile != null) {
+                showDialog(
+                  context: context,
+                  barrierDismissible:
+                      false, // Prevent user from dismissing the dialog
+                  builder: (BuildContext context) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
                     );
-                    await StorageServices()
-                        .uploadImageAndUpdateProfile(_imageFile!);
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const Profile()),
-                    );
-                  }
-                },
-                child: CircleAvatar(
-                  radius: 60, // Adjust the radius as needed
-                  backgroundImage: user!.photoURL!.isEmpty
-                      ? const AssetImage(
-                          'assets/profile_pic.png') // Placeholder image if URL is empty
-                      : NetworkImage(user!.photoURL!)
-                          as ImageProvider, // User's profile picture URL
-                  // User's profile picture URL
-                ),
-              ),
-              SizedBox(
-                height: screen.height * 0.025,
-              ),
-              Text(
-                user!.displayName!,
-                style: GoogleFonts.chewy(
-                    textStyle: const TextStyle(
-                        fontSize: 28,
-                        color: Color(0xFF000000),
-                        decoration: TextDecoration.none)),
-                textAlign: TextAlign.center,
-              ),
-            ],
+                  },
+                );
+                await StorageServices()
+                    .uploadImageAndUpdateProfile(_imageFile!);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Profile()),
+                );
+              }
+            },
+            child: CircleAvatar(
+              radius: 60, // Adjust the radius as needed
+              backgroundImage: user!.photoURL!.isEmpty
+                  ? const AssetImage(
+                      'assets/profile_pic.png') // Placeholder image if URL is empty
+                  : NetworkImage(user!.photoURL!)
+                      as ImageProvider, // User's profile picture URL
+              // User's profile picture URL
+            ),
+          ),
+          SizedBox(
+            height: screen.height * 0.025,
+          ),
+          Text(
+            user!.displayName!,
+            style: GoogleFonts.chewy(
+                textStyle: const TextStyle(
+                    fontSize: 28,
+                    color: Color(0xFF000000),
+                    decoration: TextDecoration.none)),
+            textAlign: TextAlign.center,
           ),
           SizedBox(
             height: screen.height * 0.1,
@@ -164,41 +160,37 @@ class _ProfileState extends State<Profile> {
                   ElevatedButton(
                     style: ButtonStyle(
                       backgroundColor: MaterialStateProperty.all<Color>(
-                          const Color(
-                              0xFFFBF6F0)), // Set transparent background
-                      shadowColor: MaterialStateProperty.all<Color>(
-                          Colors.transparent), // Set transparent shadow color
+                          const Color(0xFFFBF6F0)),
+                      shadowColor:
+                          MaterialStateProperty.all<Color>(Colors.transparent),
                       padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                          EdgeInsets.zero), // Remove default padding
+                          EdgeInsets.zero),
                       shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                         RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(
-                              12), // Set the border radius
+                          borderRadius: BorderRadius.circular(12),
                         ),
                       ),
                     ),
-                    child: Expanded(
-                      child: Container(
-                        padding: const EdgeInsets.only(
-                            left: 20, right: 20, top: 12, bottom: 12),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "Profile",
-                              style: GoogleFonts.chewy(
-                                  textStyle: const TextStyle(
-                                      fontSize: 22,
-                                      color: Color(0xFF6C5F57),
-                                      decoration: TextDecoration.none)),
-                              textAlign: TextAlign.center,
-                            ),
-                            const Icon(
-                              Icons.keyboard_arrow_right,
-                              color: Color(0xFF6C5F57),
-                            )
-                          ],
-                        ),
+                    child: Container(
+                      padding: const EdgeInsets.only(
+                          left: 20, right: 20, top: 12, bottom: 12),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Profile",
+                            style: GoogleFonts.chewy(
+                                textStyle: const TextStyle(
+                                    fontSize: 22,
+                                    color: Color(0xFF6C5F57),
+                                    decoration: TextDecoration.none)),
+                            textAlign: TextAlign.center,
+                          ),
+                          const Icon(
+                            Icons.keyboard_arrow_right,
+                            color: Color(0xFF6C5F57),
+                          )
+                        ],
                       ),
                     ),
                     onPressed: () {
@@ -228,28 +220,26 @@ class _ProfileState extends State<Profile> {
                         ),
                       ),
                     ),
-                    child: Expanded(
-                      child: Container(
-                        padding: const EdgeInsets.only(
-                            left: 20, right: 20, top: 12, bottom: 12),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "History",
-                              style: GoogleFonts.chewy(
-                                  textStyle: const TextStyle(
-                                      fontSize: 22,
-                                      color: Color(0xFF6C5F57),
-                                      decoration: TextDecoration.none)),
-                              textAlign: TextAlign.center,
-                            ),
-                            const Icon(
-                              Icons.keyboard_arrow_right,
-                              color: Color(0xFF6C5F57),
-                            )
-                          ],
-                        ),
+                    child: Container(
+                      padding: const EdgeInsets.only(
+                          left: 20, right: 20, top: 12, bottom: 12),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "History",
+                            style: GoogleFonts.chewy(
+                                textStyle: const TextStyle(
+                                    fontSize: 22,
+                                    color: Color(0xFF6C5F57),
+                                    decoration: TextDecoration.none)),
+                            textAlign: TextAlign.center,
+                          ),
+                          const Icon(
+                            Icons.keyboard_arrow_right,
+                            color: Color(0xFF6C5F57),
+                          )
+                        ],
                       ),
                     ),
                     onPressed: () {
@@ -293,33 +283,28 @@ class _ProfileState extends State<Profile> {
                                     ),
                                   ),
                                 ),
-                                child: Expanded(
-                                  child: Container(
-                                    padding: const EdgeInsets.only(
-                                        left: 20,
-                                        right: 20,
-                                        top: 12,
-                                        bottom: 12),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text(
-                                          "To do list",
-                                          style: GoogleFonts.chewy(
-                                              textStyle: const TextStyle(
-                                                  fontSize: 22,
-                                                  color: Color(0xFF6C5F57),
-                                                  decoration:
-                                                      TextDecoration.none)),
-                                          textAlign: TextAlign.center,
-                                        ),
-                                        const Icon(
-                                          Icons.keyboard_arrow_right,
-                                          color: Color(0xFF6C5F57),
-                                        )
-                                      ],
-                                    ),
+                                child: Container(
+                                  padding: const EdgeInsets.only(
+                                      left: 20, right: 20, top: 12, bottom: 12),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        "To do list",
+                                        style: GoogleFonts.chewy(
+                                            textStyle: const TextStyle(
+                                                fontSize: 22,
+                                                color: Color(0xFF6C5F57),
+                                                decoration:
+                                                    TextDecoration.none)),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                      const Icon(
+                                        Icons.keyboard_arrow_right,
+                                        color: Color(0xFF6C5F57),
+                                      )
+                                    ],
                                   ),
                                 ),
                                 onPressed: () {
@@ -355,24 +340,22 @@ class _ProfileState extends State<Profile> {
                         ),
                       ),
                     ),
-                    child: Expanded(
-                      child: Container(
-                        padding: const EdgeInsets.only(
-                            left: 20, right: 20, top: 12, bottom: 12),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "Logout",
-                              style: GoogleFonts.chewy(
-                                  textStyle: const TextStyle(
-                                      fontSize: 22,
-                                      color: Color(0xFF6C5F57),
-                                      decoration: TextDecoration.none)),
-                              textAlign: TextAlign.center,
-                            ),
-                          ],
-                        ),
+                    child: Container(
+                      padding: const EdgeInsets.only(
+                          left: 20, right: 20, top: 12, bottom: 12),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Logout",
+                            style: GoogleFonts.chewy(
+                                textStyle: const TextStyle(
+                                    fontSize: 22,
+                                    color: Color(0xFF6C5F57),
+                                    decoration: TextDecoration.none)),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
                       ),
                     ),
                     onPressed: () async {
